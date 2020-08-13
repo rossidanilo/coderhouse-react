e5119602f237a28392ad5d304ed5d71dccf0889c
rossidanilo <rossidanilog@gmail.com>
2020-08-13 19:32:50 -0300

    Desafío: Contador con botón

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Greeting from './components/Greeting.js';
+import ItemCount from './components/ItemCount.js';
 
-function Home () {
+const Home = function () {
 	const greeting = "Welcome to my ecommerce!";
-	return (
+	const addToCart = function(quantity){console.log('Se agregaron '+ quantity + ' unidades al carrito')}
+	return(
+		<>
 		<Greeting greeting={greeting} />
-		);
+		<ItemCount max={6} min={2} initial={2} onAdd={addToCart}/>
+		</>)
 }
 
 export default Home;
