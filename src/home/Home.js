36a958b889e3ac8e5f22f5fe8b6f6831b2a164e4
rossidanilo <rossidanilog@gmail.com>
2020-08-15 13:22:56 -0300

    Desafío: Catálogo con MAPS y Promises

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Greeting from './components/Greeting.js';
-import ItemCount from './components/ItemCount.js';
+import ItemList from './components/ItemList.js';
 
 const Home = function () {
 	const greeting = "Welcome to my ecommerce!";
-	const addToCart = function(quantity){console.log('Se agregaron '+ quantity + ' unidades al carrito')}
 	return(
 		<>
 		<Greeting greeting={greeting} />
-		<ItemCount max={6} min={2} initial={2} onAdd={addToCart}/>
+		<ItemList />
 		</>)
 }
 
