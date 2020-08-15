5a67096d9ede5565c7e180bc97cdad43e08c0063
rossidanilo <rossidanilog@gmail.com>
2020-08-15 13:23:26 -0300

    Desafío: Catálogo con MAPS y Promises

--- src/home/components/ItemCount.js
+++ src/home/components/ItemCount.js
@@ -1,25 +1,12 @@
 import React, {useState} from 'react';
-import ProductImage from '../../assets/car-spark-plug.jpg';
 
 const style = {
-	card: {
-		marginTop: 20,
-		width: '30%',
-		marginRight: 'auto',
-		marginLeft: 'auto',
-	},
 	icon: {
 		fontSize : 14,
 	},
 	text: {
 		fontSize: 18,
 		fontWeight: 'bold',
-	},
-	image: {
-		width: 200,
-		heigth: 200,
-		marginRight: 'auto',
-		marginLeft: 'auto',
 	},
 }
 
@@ -37,29 +24,26 @@ const ItemCount = function({ max, min, initial, onAdd }){
 
 	return(
 		<>
-			<div className="card" style={style.card}>
-  				<img style={style.image} className="card-img-top" src={ProductImage} alt="product-image"/>
-  				<div className="card-body">
-    				<div className="row">
-  						<div className="col">
-  							<a onClick={removeItem} className="btn btn-info">
-  								<i style={style.icon} class="fa fa-minus"></i>
-  							</a>
-  						</div>
-  						<div className="col">
-  							<p style={style.text}>{ counter }</p>
-  						</div>
-  						<div className="col">
-  							<a onClick={addItem} className="btn btn-info">
-  								<i style={style.icon} class="fa fa-plus"></i>
-  							</a>
-  						</div>
+  			<div className="card-body">
+    			<div className="row">
+  					<div className="col">
+  						<a onClick={removeItem} className="btn btn-info">
+  							<i style={style.icon} class="fa fa-minus"></i>
+  						</a>
+  					</div>
+  					<div className="col">
+  						<p style={style.text}>{ counter }</p>
+  					</div>
+  					<div className="col">
+  						<a onClick={addItem} className="btn btn-info">
+  							<i style={style.icon} class="fa fa-plus"></i>
+  						</a>
   					</div>
   				</div>
-  				<div className="card-footer">
-  					<button onClick={ () => onAdd(counter) } className="btn btn-info">Agregar al carrito</button>
-  				</div>
-			</div>
+  			</div>
+  			<div className="card-footer">
+  				<button onClick={ () => onAdd(counter) } className="btn btn-info">Agregar al carrito</button>
+  			</div>
 		</>
 		);
 }
